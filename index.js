5dc618421ca1ab1c336dc5ebcccdc9e5f809130d
Bogdan Paliy <bpalij@gmail.com>
2016-12-02 16:54:15 +0200

    Change buttons

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
-$('#rst-btn').mouseenter(function (){
-	$(this).addClass("btn-danger");
-	$(this).removeClass("btn-warning");
-});
-$('#rst-btn').mouseleave(function (){
-	$(this).removeClass("btn-danger");
-	$(this).addClass("btn-warning");
-});
-$('#start-button').mouseenter(function (){
-	$(this).addClass("btn-primary");
-	$(this).removeClass("btn-success");
-});
-$('#start-button').mouseleave(function (){
-	$(this).removeClass("btn-primary");
-	$(this).addClass("btn-success");
-});
+
 function generate(){
 	$(".wait").css("visibility", "visible");
 	var originStr = $("#input-field").val();
